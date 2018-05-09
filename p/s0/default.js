c441adf4a5df7856c4c46aabef3cfe51276e2535
Yu Changyuan <reivzy@gmail.com>
2018-05-09 18:20:59 +0800

    Rename donation0 to s0 and finish page.

---
+++ p/s0/default.js
@@ -0,0 +1,166 @@
+var _tgt = "http://127.0.0.1:3000/s0";
+
+var _s0 = {};
+
+_s0.update_stat = function(list) {
+    var tbl_todo = $("#tbl-todo");
+    var tbl_done = $("#tbl-done");
+
+    tbl_todo.empty();
+    tbl_done.empty();
+
+    var total = 0;
+
+    function add_row(tbl, tag, name, email, value, date, code) {
+        var n1 = name;
+        var e1 = email;
+
+        function format_dt(dt) {
+            function pad(s) {
+                if (s.length < 2) s = '0' + s;
+                return s;
+            }
+
+            month = pad('' + (dt.getMonth() + 1));
+            day = pad('' + dt.getDate());
+            year = pad('' + dt.getFullYear());
+
+            hr  = pad('' + dt.getHours());
+            min = pad('' + dt.getMinutes());
+
+            return [year, month, day].join('-') + ' ' +
+                [hr, min].join(':');
+        }
+
+        var t = date;
+        if (Number.isInteger(t)) {
+            var dt = new Date(date);
+
+            t = $("<span>")
+                .text(format_dt(dt))
+                .css("font-family", "monospace");
+        }
+
+        if (n1 == "") { n1 = $("<em>").css("color", "gray").text("（匿名）"); }
+        if (e1 == "") { e1 = $("<em>").css("color", "gray").text("（匿名）"); }
+
+        tbl.append($('<tr>')
+                   .append($(tag).append(n1))
+                   .append($(tag).append(e1))
+                   .append($(tag).text(value))
+                   .append($(tag).append(t))
+                   .append($(tag).text(code)));
+
+    };
+
+    add_row(tbl_todo, '<th>', "姓名", "邮箱", "金额", "时间", "捐款码");
+    add_row(tbl_done, '<th>', "姓名", "邮箱", "金额", "时间", "捐款码");
+
+    // sort by date, reversed
+    list.sort(function (a, b) {
+        return (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0);
+    });
+
+    list.forEach(function(l) {
+        if (l.confirmed) {
+            add_row(tbl_done, '<td>', l.name, l.email, l.value, l.date, l.code);
+
+            total += l.value;
+        }
+        else {
+            add_row(tbl_todo, '<td>', l.name, l.email, l.value, l.date, l.code);
+        }
+        console.log(l);
+    });
+
+    $("#total-number").text(total + " / 256");
+
+    function set_style(tbl) {
+        tbl.find("td,th").css("border", "1px solid black");
+        tbl.css("border-collapse", "collapse")
+            .css("width", "100%");
+
+
+        tbl.find("td,th").css("padding", "0 1em 0 1em");
+        tbl.find("td,th").css("text-align", "center");
+    };
+
+    set_style(tbl_todo);
+    set_style(tbl_done);
+
+};
+
+$(function() {
+    $( "#donate-form" ).submit(function(e) {
+        e.preventDefault();
+
+        var name = $("#name").val();
+        var email = $("#email").val();
+
+        $("#donate-form input").attr("disabled", "disabled");
+        $("#submit").attr("value", "载入中...");
+
+        // alert("name = " + name + ", email = " + email);
+
+        $.ajax({
+            url: _tgt + "/j",
+            type: "get",
+            data: { "name": name, "email": email },
+            datatype: 'json',
+            success: function(data) {
+                var succ = data.succ;
+
+                console.log(data.succ);
+                console.log(data.curr);
+
+                var n0 = $("<ul>")
+                    .append($("<li>").text("姓名: " + data.curr.name))
+                    .append($("<li>").text("邮箱: " + data.curr.email))
+                    .append($("<li>").text("IP: " + data.curr.ip))
+                    .append($("<li>").text("金额： " + data.curr.value))
+                    .append($("<li>").text("捐款码： ")
+                            .append($("<span>").text(data.curr.code)
+                                    .css("color", "black")
+                                    .css("font-family", "monospace")
+                                    .css("font-weight", "normal")));
+
+                var n1 = $("<div>").append(n0);
+
+                if (!data.succ) {
+                    n1.append($("<p>")
+                              .css("font-style", "italic")
+                              .css("color", "red")
+                              .text("已有记录，提交失败。请修改姓名或邮箱，若匿名则请更换IP"));
+                }
+
+                $("#donate-form").replaceWith(n1);
+
+                _s0.update_stat(data.list);
+
+                // val = x;
+                // id = x;
+                // stat = x
+
+            },
+            error:function(){
+                alert("Error");
+            }
+        });
+
+    });
+});
+
+$(function () {
+    $.ajax({
+        url: _tgt,
+        type: "get",
+        datatype: 'json',
+        success: function(data) {
+            console.log(data);
+            _s0.update_stat(data.list);
+        },
+        error:function(){
+            alert("Error");
+        }
+    });
+});
---
+++ p/s0/index.md
@@ -0,0 +1,65 @@
+---
+layout: page
+title: 大家来给SHLUG捐域名费吧～
+permalink: /p/s0/
+---
+<script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="default.js"></script>
+
+## ***NOTE: this is draft***{:style="color:red"}
+
+## 我们现在的情况
+
+目前，SHLUG除了人力外，最大的支出即我们目前持有的shlug.org、shlug.net和shlug.com三个域名的域名费。
+
+前几年一直是托管在GoDaddy上面的，每年的费用需要`¥500`左右，一直由我们SHLUG的“离休干部”geekbone同学个人资助。我觉得，通过社区成员的捐助，而非个人的资助（此处应有感谢），来实现社区的运转，是更为良好和健康的。因此呢，在这里我号召大家捐款来交今年我们LUG的域名费。
+
+哦，忘记说了，由于GoDaddy价格实在感人，今年我们把域名全部迁移到Google Domains上面了，三个域名一共`$36/yr`，迁移是在三月完成的，暂时由geekbone同学垫付的（加元支付的，`51 CAD`）。
+
+|![付款截图](ns_fee_2018.jpg)|
+|:--:|
+|*付款截图*|
+
+## 捐款规则
+
+今年的域名费捐款，我们取个整，目标就暂定`0x100`元吧。至于每人捐款的数目，也取整数，就随机`2'b10`、`0b100`、`010`和`0x10`元吧。最后的捐款数量可能会超过目标，超过后看实际情况停止接受捐款，然后将筹得的所有捐款转给geekbone同学。
+
+## 我要捐款
+
+> 填好下面的表单后（可匿名），请点击“捐款”。随后页面会更新，表单的内容会替换为用于支付捐款的二维码（微信和支付宝各一个）和一个长度为3的字符串（捐款码）。需要注意的是，捐款的金额是随机生成的。*支付时，请在备注里面填写该字符串*{:style="color:red"}，否则捐款将作为匿名捐款处理。
+
+> 支付捐款后，会有人工进行确认，并更新捐款情况。请耐心等待～
+
+> 后台服务器代码见： 
+
+<form id="donate-form" action="http://127.0.0.1:3000/s0/h">
+<table>
+<tr>
+<td>姓名或昵称（可选）</td>
+<td><input type="text" id="name" name="name"/></td>
+</tr>
+
+<tr>
+<td>邮箱（可选）</td>
+<td><input type="text" id="email" name="email"/></td>
+</tr>
+</table>
+
+<p></p>
+<p><input type="submit" id="submit" value="捐款"/></p>
+</form>
+
+## 捐款情况
+
+### 已确认
+
+总金额： <span id="total-number"></span>
+
+<table id="tbl-done" style="border:1px;"><tbody></tbody></table>
+
+### 待确认
+
+> 待确认记录会定期进行人工清理，请及时支付
+
+<table id="tbl-todo"><tbody></tbody></table>
+
